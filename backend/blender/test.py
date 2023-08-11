5e55600e612dbaed9a6e8ff981c060a3767d7e7f
Dominic <lovric.domagoj1@gmail.com>
2023-08-11 22:00:18 +0100

    Updated test file

--- backend/blender/test.py
+++ backend/blender/test.py
@@ -1,8 +1,10 @@
 import bpy
+import sys
+sys.path.append("/")
+
+from objectConfigurator.loadBlenderTemplate import loadBlendERTemplate
+
 
 print("Hello, World!")
-
-
-import objectConfigurator
 
 loadBlendERTemplate()
