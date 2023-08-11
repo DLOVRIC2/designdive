35eadff9af5201f20823c45d381c8d7f2a811b21
Dominic <lovric.domagoj1@gmail.com>
2023-08-11 22:36:12 +0100

    Updated file path

--- backend/blender/objectConfigurator/loadBlenderTemplate.py
+++ backend/blender/objectConfigurator/loadBlenderTemplate.py
@@ -5,5 +5,5 @@ sys.path.append("/")
 
 #this function opens blender file loaded on repository which will serve as configurator
 def openBlenderTemplateFile():
-    bpy.ops.wm.open_mainfile(filepath="templateFile.blend")
+    bpy.ops.wm.open_mainfile(filepath="objectConfigurator/templateFile.blend")
     
