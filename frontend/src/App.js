76d160068134c895d6cb20734f31b07318986214
Dominic <lovric.domagoj1@gmail.com>
2023-08-12 18:29:31 +0100

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./features/Home";
+import Generate from "./features/Generate"; // Import the Generate component
+
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/generate" element={<Generate />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
