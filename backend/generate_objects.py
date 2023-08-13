0a0dd8496ec45c5b8660524e07a6fcd95a69ff1e
Dominic <lovric.domagoj1@gmail.com>
2023-08-13 14:52:24 +0100

    Added logging

--- backend/generate_objects.py
+++ backend/generate_objects.py
@@ -5,6 +5,9 @@ import json
 import requests
 from typing import List
 import re
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ReplicateObjectGenerator:
 
@@ -16,8 +19,8 @@ class ReplicateObjectGenerator:
         self.blender_dir = os.path.join(self.current_file, "blender/objects_3d")
         load_dotenv(os.path.join(self.root_dir, ".env"))
         self.generated_objects = {}
+        self.replicate_api_key = os.environ.get("REPLICATE_API_TOKEN")
 
-        self.replicate_api_key = os.environ.get("REPLICATE_API_PROFILE")
         if not self.replicate_api_key:
             raise ValueError("You must provide a replicate API key.")
         else:
@@ -28,6 +31,7 @@ class ReplicateObjectGenerator:
         """Download processed objects."""
         response = requests.get(url)
         if response.status_code == 200:
+            logger.info("Status code == 200!")
             with open(file_path, "wb") as obj_file:
                 obj_file.write(response.content)
             print(f"{file_path} downloaded")
@@ -36,6 +40,7 @@ class ReplicateObjectGenerator:
 
     def _generate_object(self, prompt: str) -> List:
         """Calls replicate API to generate .gif and .obj"""
+        logger.info(f"Running the Replicate api call with the following prompt: {prompt}")
         return replicate.run(
             "cjwbw/shap-e:5957069d5c509126a73c7cb68abcddbb985aeefa4d318e7c63ec1352ce6da68c",
             input={"prompt": prompt, "save_mesh": True}
@@ -44,16 +49,18 @@ class ReplicateObjectGenerator:
     def _process_object(self, item: str, save_path: str):
         """Runs the api call to generate object and downloads the .obj file in a folder for blender processing"""
         output = self._generate_object(item)
+        logger.info(f"Sucessfully generated {item}")
         
         # Create a valid file name
-        valid_name = re.sub(r'[^a-zA-Z0-9]', '_', item.lower())
+        valid_name = re.sub(r'[^a-zA-Z0-9]', '_', item.lower()) + "_0000"
+        logger.info("Saved it under the name {valid_name}")
 
         self.generated_objects[valid_name] = {"gif": output[0], "obj": output[1]}
-        print(f"{item} has been generated!")
 
         # Save the file
         obj_file_name = f"{valid_name}.obj"
         obj_file_path = os.path.join(save_path, obj_file_name)
+        logger.info(f"Saving the file to the following location: {obj_file_path}")
         self._download_obj_file(output[1], obj_file_path)
 
     def generate_temp_files(self, objects: List[str]):
@@ -75,24 +82,30 @@ class ReplicateObjectGenerator:
         with open(json_file_path, "w") as json_file:
             json.dump(self.generated_objects, json_file, indent=4)
 
-        print("Generated objects saved to", json_file_path)
+        logger.info(f"Generated objects saved to {json_file_path}")
 
-
-    def generate_user_files(self, prompt):
+    def generate_user_files(self, prompts: List[str]):
         """Generates an object """
-        self._process_object(prompt, save_path=self.user_gen_dir)
 
-        json_file_path = os.path.join(self.root_dir, "generated_objects.json")
+        logger.info(f"Received a total of {len(prompts)} prompts.")
+        if len(prompts) != 0:
+            for prompt in prompts:
+                self._process_object(prompt, save_path=self.user_gen_dir)
 
-        with open(json_file_path, "w") as json_file:
-            json.dump(self.generated_objects, json_file, indent=4)
+                json_file_path = os.path.join(self.root_dir, "generated_objects.json")
 
-        print("Generated objects saved to", json_file_path)
+                with open(json_file_path, "w") as json_file:
+                    json.dump(self.generated_objects, json_file, indent=4)
 
-# Usage example
-generator = ReplicateObjectGenerator()
-fixed_objects = ["lamp", "rug", "television", "mirror", "plant", "clock", "cabinet", "fireplace"]
-user_object = ["A chair that looks like an avocado"]
+                print("Generated objects saved to", json_file_path)
 
-generator.generate_temp_files(fixed_objects)
-# generator.generate_user_files(user_object)
+
+
+if __name__ == "__main__":
+    # Usage example
+    generator = ReplicateObjectGenerator()
+    fixed_objects = ["lamp", "rug", "television", "mirror", "plant", "clock", "cabinet", "fireplace"]
+    user_object = ["A chair that looks like an avocado"]
+
+    generator.generate_temp_files(fixed_objects)
+    # generator.generate_user_files(user_object)
