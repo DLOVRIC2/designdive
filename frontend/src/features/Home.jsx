83a33c6d47db7f560d863407bcac0bf32b152858
Dominic <lovric.domagoj1@gmail.com>
2023-08-13 09:31:39 +0100

    Update on the location of pricetable

--- frontend/src/features/Home.jsx
+++ frontend/src/features/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Body from './Body'
 import Footer from './Footer'
+import Pricetable from './Pricetable';
+
 
 const Home = () => {
   return (
     <div>
       <Body/>
+      <Pricetable/>
       <Footer/>
     </div>
   )
