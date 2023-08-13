7d9b8940805572aca4e8c89ce421b46df18d4080
Dominic <lovric.domagoj1@gmail.com>
2023-08-13 14:52:29 +0100

    Updated api

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,10 @@ from generate_objects import ReplicateObjectGenerator
 import time
 import os
 import logging
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Body
+from typing import List
+
 
 current_path = os.path.dirname(__file__)
 blender_path = os.path.join(current_path, "blender")
@@ -14,10 +18,26 @@ app = FastAPI()
 logger = logging.getLogger("uvicorn")
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 tasks = {}
 
 def process_task(prompt, task_id):
+    logger.info("I'm about to start this generator")
     generator = ReplicateObjectGenerator()
+    print(prompt)
     generator.generate_user_files(prompt)
 
     # TODO: We need to add blender processing here
@@ -26,11 +46,11 @@ def process_task(prompt, task_id):
     tasks[task_id]['result'] = user_download_path
 
 @app.post("/start_task/")
-async def start_task(prompt: str, background_tasks: BackgroundTasks):
-    logger.info(f"Received start_task request with prompt: {prompt}")
+async def start_task(background_tasks: BackgroundTasks, prompts: List[str] = Body(...)):
+    logger.info(f"Received start_task request with prompts: {prompts}")
     task_id = str(len(tasks))
     tasks[task_id] = {'status': 'running'}
-    background_tasks.add_task(process_task, prompt, task_id)
+    background_tasks.add_task(process_task, prompts, task_id)
     logger.info(f"Started task with ID: {task_id}")
     return {"status": "success", "task_id": task_id}
 
