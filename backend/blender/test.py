92e42581195ce880cf9c125c77cce2674133d451
Dominic <lovric.domagoj1@gmail.com>
2023-08-13 20:14:21 +0100

    Updated test file

--- backend/blender/test.py
+++ backend/blender/test.py
@@ -1,25 +1,33 @@
 import bpy
 import sys
-sys.path.append("/")
-
+import os
+sys.path.append(os.path.dirname(__file__))
 from objectConfigurator.blenderManipulation import openBlenderTemplateFile, renderCamera, objectProcess
-
 from objectConfigurator.fileManipulation import getObject, itemCategoryCheck
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+import json
+
+current_directory = os.path.dirname(__file__)
+user_input_path = os.path.join(current_directory, "user_input", "user_input.json")
+with open(user_input_path, "r") as f:
+    params = json.load(f)
+ 
+user_defined_categories = params["user_defined_categories"]
+room_category = params["room_type"]
+logger.info(f"User defined categories loaded in are: {user_defined_categories}")
+logger.info(f"User defined room type loaded in is: {room_category}")
 
 
-user_defined_categories = ["table","fireplace"]
 
-
-
-openBlenderTemplateFile("livingroom")
-
+openBlenderTemplateFile(roomCategory=room_category[0])
+logger.info("Just set 'openBlenderTemplateFile'")
 pregenerated = itemCategoryCheck(user_defined_categories)
-print(pregenerated)
-
-#objectPath = getObject(category)
-
-
+logger.info(f"pregenerated categories are {pregenerated}")
+logger.info("Starting the processing")
 objectProcess(user_defined_categories, pregenerated)
+logger.info("Rendering the camera")
 renderCamera("render_test")
 
 
