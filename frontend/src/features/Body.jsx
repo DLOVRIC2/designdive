602ba998b983dbfea962b129b5d0e9618e076c72
Dominic <lovric.domagoj1@gmail.com>
2023-08-14 07:44:48 +0100

    Updated on body content

--- frontend/src/features/Body.jsx
+++ frontend/src/features/Body.jsx
@@ -1,37 +1,66 @@
 import React from 'react';
-import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
-import { ArrowRight } from 'react-feather';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+// import './Style/Body.css'
 
 const Body = () => {
+  const navigate = useNavigate(); // Initialize useNavigate hook
+
+  const handleStartCustomizing = () => {
+    navigate('/generate'); // Navigate to the "Generate" page
+  };
+
   return (
     <div className="container">
       <div className="row text-center">
         <div className="col">
           <h1 className="title">
-            Turn <span className="gradient-text">One video</span><br />
-            Into<span className="gradient-text"> 5 Viral Clips</span>
+            Create <span className="gradient-text">Stunning Scenes</span><br />
+            With <span className="gradient-text">Customized 3D Models</span>
           </h1>
           <h5 className='fw-normal mb-4 text-secondary'>
-            Boost your visibility with our AI-powered Viral Content Generator <br />
-            across all platforms - YouTube, TikTok, Facebook, and beyond.
+            Transform your living spaces with our AI-powered 3D Model Customizer. <br />
+            Choose themes, add effects, and make your imagination come alive.
           </h5>
         </div>
       </div>
-      <div className="row justify-content-center align-items-center">
-        <div className="col-md-6 col-lg-6 mb-3 mb-md-0">
-          <TextField id="link-field" label="Paste your link here" fullWidth variant="outlined" />
+      <div className="row text-center mt-4">
+        <div className="col-md-4">
+          <h4>Choose Scene</h4>
+          <p>Select from available scenes like the 'Living Room', 'Bedroom' or 'Office Space' and add any item you can imagine!.</p>
         </div>
-        <div className="col-md-4 col-lg-2 mt-3 mt-md-0">
-          <Button variant="outlined" className='btn-grad text-light w-100 p-2 rounded'>
-            Make clip <ArrowRight/>
-          </Button>
+        <div className="col-md-4">
+          <h4>Add Effects</h4>
+          <p>Apply cool effects, materials, and shadows to create stunning visuals.</p>
+        </div>
+        <div className="col-md-4">
+          <h4>Download & Share</h4>
+          <p>Download the images of your ideal scene and share them across social media platforms.</p>
         </div>
       </div>
-      <div className='row justify-content-center mt-3'>
-        <div className='col-md-4 text-center'>
-          <Button variant="outlined" className='btn-grad text-light w-10 rounded w-lg-50'>
-            Load Demo
+
+      <div className="row text-center mt-4">
+        <div className="col-md-4">
+          <h4>3D Object Generation</h4>
+          <p>Create unique 3D objects based on simple text descriptions and bring your vision to life.</p>
+        </div>
+        <div className="col-md-4">
+          <h4>Blender Rendering</h4>
+          <p>Utilize advanced rendering technology to give your designs a realistic and professional appearance.</p>
+        </div>
+        <div className="col-md-4">
+          <h4>User Customization</h4>
+          <p>Adjust, modify, and personalize your 3D models to create the perfect representation of your idea.</p>
+        </div>
+      </div>
+      <div className="row text-center mt-5">
+        <div className="col">
+          <Button
+            variant="contained"
+            className='btn-grad text-light w-100 p-2 rounded'
+            onClick={handleStartCustomizing}
+          >
+            Start Customizing
           </Button>
         </div>
       </div>
