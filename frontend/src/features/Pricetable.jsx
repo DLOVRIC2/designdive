eca459a55f984f33ab7acd287c8a6bc0950998aa
Dominic <lovric.domagoj1@gmail.com>
2023-08-14 07:45:18 +0100

    Updated pricetable

--- frontend/src/features/Pricetable.jsx
+++ frontend/src/features/Pricetable.jsx
@@ -40,58 +40,61 @@ const Pricetable = () => {
       </header>
 
       <div className="row justify-content-center">
-        <div className="card shadow col-md-3 m-1">
-          <ul className="list-unstyled text-center">
-            <li className="pack">Basic</li>
-            <li
-              id="basic"
-              className="price bottom-bar text-secondary"
-            >
-              {annualBilling ? '$19.99' : '$199.99'}
-            </li>
-            <li className="bottom-bar">150 minutes of video upload monthly</li>
-            <li className="bottom-bar">1080p HD rendering</li>
-            <li className="bottom-bar"> 50GB storage</li>
-            <li>
-              <button className="btn btn-primary">Subscribe</button>
-            </li>
-          </ul>
-        </div>
-        <div className="card active col-md-3 m-1">
-          <ul className="list-unstyled text-center">
-            <li className="pack">Professional</li>
-            <li
-              id="professional"
-              className="price bottom-bar text-secondary"
-            >
-              {annualBilling ? '$24.99' : '$249.99'}
-            </li>
-            <li className="bottom-bar"> 500 video upload minutes monthly
-</li>
-            <li className="bottom-bar">Priority Support</li>
-            <li className="bottom-bar"> Brand Template Builder</li>
-            <li>
-              <button className="btn btn-primary active-btn">Subscribe</button>
-            </li>
-          </ul>
-        </div>
-        <div className="card shadow col-md-3 m-1">
-          <ul className="list-unstyled text-center">
-            <li className="pack">Master</li>
-            <li
-              id="master"
-              className="price bottom-bar text-secondary"
-            >
-              {annualBilling ? '$39.99' : '$399.99'}
-            </li>
-            <li className="bottom-bar">Custom Intros, Outros & Fonts</li>
-            <li className="bottom-bar">10 Users Allowed</li>
-            <li className="bottom-bar">Brand Template Builder</li>
-            <li>
-              <button className="btn btn-primary">Subscribe</button>
-            </li>
-          </ul>
-        </div>
+      <div className="card shadow col-md-3 m-1">
+        <ul className="list-unstyled text-center">
+          <li className="pack">Basic</li>
+          <li
+            id="basic"
+            className="price bottom-bar text-secondary"
+          >
+            {annualBilling ? '$199.99' : '$19.99'}
+          </li>
+          <li className="bottom-bar">5 Text-to-3D Object Conversions per Month</li>
+          <li className="bottom-bar">Access to Predefined Scenes</li>
+          <li className="bottom-bar">Standard Blender Rendering</li>
+          <li>
+            <button className="btn btn-primary">Subscribe</button>
+          </li>
+        </ul>
+      </div>
+      <div className="card active col-md-3 m-1">
+        <ul className="list-unstyled text-center">
+          <li className="pack">Professional</li>
+          <li
+            id="professional"
+            className="price bottom-bar text-secondary"
+          >
+            {annualBilling ? '$249.99' : '$24.99'}
+          </li>
+          <li className="bottom-bar">Unlimited Text-to-3D Object Conversions</li>
+          <li className="bottom-bar">Access to Premium Scenes</li>
+          <li className="bottom-bar">Advanced Blender Rendering Options</li>
+          <li className="bottom-bar">Basic User Customization Tools</li>
+          <li>
+            <button className="btn btn-primary active-btn">Subscribe</button>
+          </li>
+        </ul>
+      </div>
+      <div className="card shadow col-md-3 m-1">
+        <ul className="list-unstyled text-center">
+          <li className="pack">Master</li>
+          <li
+            id="master"
+            className="price bottom-bar text-secondary"
+          >
+            {annualBilling ? '$399.99' : '$39.99'}
+          </li>
+          <li className="bottom-bar">Unlimited Text-to-3D Object Conversions</li>
+          <li className="bottom-bar">Access to All Scenes, Including Premium</li>
+          <li className="bottom-bar">Full User Customization & Personalization</li>
+          <li className="bottom-bar">Premium Support & Collaboration Tools</li>
+          <li className="bottom-bar">Exclusive Access to Upcoming Features</li>
+          <li>
+            <button className="btn btn-primary">Subscribe</button>
+          </li>
+        </ul>
+      </div>
+
     </div>
     </div>
   );
